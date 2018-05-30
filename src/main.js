87aaa84e15092c30527e34a6ade247668d7c89b0
Patrick Mazulo <pmazulo@gmail.com>
2018-05-29 22:20:26 -0300

    add movie filter

--- src/main.js
+++ src/main.js
@@ -2,9 +2,51 @@ import Vue from 'vue';
 
 import './style.scss';
 
+import genres from './util/genres';
+
 new Vue({
     el: '#app',
-    data: {
-        msg: 'Hello world!',
+    components: {
+        'movie-list': {
+            template: `
+                <div id="movie-list">
+                    <div class="movie" v-for="movie in movies">
+                        {{ movie.title }}
+                    </div>
+                </div>
+            `,
+            data: function() {
+                return {
+                    movies: [
+                        { title: 'Movie 1'},
+                        { title: 'Movie 2'},
+                        { title: 'Movie 3'},
+                    ]
+                };
+            },
+        },
+        'movie-filter': {
+            data() {
+                return {
+                    genres
+                }
+            },
+            template: `
+                <div id="movie-filter">
+                    <h2>Movie filter</h2>
+                    <div class="filter-group">
+                        <check-filter v-for="genre in genres" v-bind:genre="genre"></check-filter>
+                    </div>
+                </div>
+            `,
+            components: {
+                'check-filter': {
+                    props: ['genre'],
+                    template: `
+                        <div>{{ genre }}</div>
+                    `,
+                }
+            }
+        },
     },
 });
