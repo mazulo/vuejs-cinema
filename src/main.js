d0643d832f09c5daf7b13dc1334abe0c01780dd4
Patrick Mazulo <pmazulo@gmail.com>
2018-05-29 23:58:00 -0300

    Send data through events from child to parent

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ import genres from './util/genres';
 
 new Vue({
     el: '#app',
+    data: {
+        genre: [],
+        time: [],
+    },
+    methods: {
+        checkFilter(category, title, checked) {
+            console.log(category, title, checked);
+            if (checked) {
+                this[category].push(title);
+            } else {
+                let index = this[category].indexOf(title);
+                if (index > -1) {
+                    this[category].splice(index, 1);
+                }
+            }
+        },
+    },
     components: {
         'movie-list': {
             template: `
@@ -15,13 +32,13 @@ new Vue({
                     </div>
                 </div>
             `,
-            data: function() {
+            data() {
                 return {
                     movies: [
                         { title: 'Movie 1'},
                         { title: 'Movie 2'},
                         { title: 'Movie 3'},
-                    ]
+                    ],
                 };
             },
         },
@@ -35,10 +52,15 @@ new Vue({
                 <div id="movie-filter">
                     <h2>Movie filter</h2>
                     <div class="filter-group">
-                        <check-filter v-for="genre in genres" :title="genre"></check-filter>
+                        <check-filter v-for="genre in genres" :title="genre" v-on:check-filter="checkFilter"></check-filter>
                     </div>
                 </div>
             `,
+            methods: {
+                checkFilter(category, title, checked) {
+                    this.$emit('check-filter', category, title, checked);
+                },
+            },
             components: {
                 'check-filter': {
                     data() {
@@ -48,13 +70,19 @@ new Vue({
                     },
                     props: ['title'],
                     template: `
-                        <div v-bind:class="{ 'check-filter': true, 'active': checked }" v-on:click="checked = !checked">
+                        <div v-bind:class="{ 'check-filter': true, 'active': checked }" v-on:click="checkFilter">
                             <span class="checkbox"></span>
                             <span class="check-filter-title">
                                 {{ title }}
                             </span>
                         </div>
                     `,
+                    methods: {
+                        checkFilter() {
+                            this.checked = !this.checked;
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
+                        },
+                    },
                 }
             }
         },
